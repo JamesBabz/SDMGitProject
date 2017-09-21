1f609ccb7a5576b2f93566694b9716ed7c9b7c57
Enemark96 <je.enemark96@gmail.com>
2017-09-21 13:10:27 +0200

    Entities
    Entities for Category, Project Request, StudentGroup

---
+++ ClassLibrary1/BE/StudentGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.BE
+{
+    class StudentGroup
+    {
+        public int StudentGroupID { get; set; }
+
+        public string Name { get; set; }
+
+        public int StudentID { get; set; }
+
+        public string Semester { get; set; }
+
+
+
+    }
+}
