0a4df2b767f4af7d0b66d7f5f375baa6526cc715
JamesBabz <web.fuhlendorff@gmail.com>
2017-09-21 14:15:06 +0200

    added enitities
    Student
    Supervisor
    SupervisorRequest

---
+++ ClassLibrary1/BE/SupervisorRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.BE
+{
+    class SupervisorRequest
+    {
+        public int SupervisorId { get; set; }
+        public int GroupId { get; set; }
+        public int AdminId { get; set; }
+        public bool IsAccepted { get; set; }
+    }
+}
