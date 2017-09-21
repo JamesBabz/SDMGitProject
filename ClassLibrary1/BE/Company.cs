58a1e32afd31802434c88a2a43e66f88cf65255b
thomasmeyerh <thom953b@easv365.dk>
2017-09-21 12:56:29 +0200

    BE - Company, profile, project
    BE - Company, profile, project

---
+++ ClassLibrary1/BE/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.BE
+{
+    public class Company
+    {
+        public string Name { get; set; }
+
+        public int Cvr { get; set; }
+
+        public string Adress { get; set; }
+
+        public int Phone { get; set; }
+
+        public string Mail { get; set; }
+
+        public string ContactPerson { get; set; }
+    }
+}
