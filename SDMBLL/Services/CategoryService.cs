4cae556ae7391a2747136c4d416fa97dfa7f232d
Enemark96 <je.enemark96@gmail.com>
2017-09-29 11:47:38 +0200

    Fixed last
    Fixed last

--- SDMBLL/Services/CategoryService.cs
+++ SDMBLL/Services/CategoryService.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 using SDMBLL.InterfaceServices;
+using SDMDAL;
+using SDMDAL.InterfaceRepositories;
 using SDMEntity.BE;
 
 namespace SDMBLL.Services
 {
         public class CategoryService : ICategoryService
-    {
+        {
+
+            private ICategoryRepository repo = new DALFacade().CategoryRepository; 
+
+
         public Category Create(Category category)
         {
+            return repo.Create(category);
         }
 
         public Category Delete(int id)
