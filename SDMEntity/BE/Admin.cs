e7be4708808c4d8d0b38cdc3480c101ac30c8139
thomasmeyerh <thom953b@easv365.dk>
2017-10-26 12:16:15 +0200

    Fixed Admin

--- SDMEntity/BE/Admin.cs
+++ SDMEntity/BE/Admin.cs
@@ -6,7 +6,6 @@ namespace SDMEntity.BE
 {
     public class Admin
     {
-        Sigurd og kurt
         public int Id {get; set; }
         public string Name {get; set; }
         public int PhoneNumber { get; set; }
